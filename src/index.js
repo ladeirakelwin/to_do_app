336665724ee01ff0a738a1313d176046ccca39a8
ladeirakelwin <ladeirakelwin@gmail.com>
2020-09-17 23:34:37 -0300

    adicionando cors e middleware de página staticas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const app = require('./custom-express')
 const route = require('./routes/routes')
 const port = 3000
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
-
+app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static('public'));
 
 app.listen(port, ()=> console.log("Funfo!"))
 
